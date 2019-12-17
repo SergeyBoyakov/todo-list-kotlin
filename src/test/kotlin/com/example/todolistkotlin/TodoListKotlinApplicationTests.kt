09230ac1a0da9dc27906a13c4c2a666bf104bceb
sb <sergeyboyakov@gmail.com>
2019-12-17 18:16:36 +0300

    Add dependencies, main class

---
+++ src/test/kotlin/com/example/todolistkotlin/TodoListKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.todolistkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoListKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
