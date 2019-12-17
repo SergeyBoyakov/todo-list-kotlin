7a2d90621176e3f7a7a6035ea6af340d021f9645
sb <sergeyboyakov@gmail.com>
2019-12-17 19:48:18 +0300

    Implement card repository
    For now implemented with JPA entity manager. In future will be migrated
    to Spring Data Jpa or Spring Data Mongo

--- src/main/kotlin/com/example/todolistkotlin/model/Card.kt
+++ src/main/kotlin/com/example/todolistkotlin/model/Card.kt
@@ -1,17 +1,35 @@
 package com.example.todolistkotlin.model
 
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.util.*
 import javax.persistence.*
 
 
 @Entity
+@Table(name = "cards")
+@EntityListeners(AuditingEntityListener::class)
 data class Card(
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    val id: Long,
+
 
     @Column(name = "title")
     val title: String,
 
     @Column(name = "description")
     val description: String
-)
+) {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Long? = null
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    lateinit var createdDate: Date
+
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    lateinit var updatedDate: Date
+}
--- src/main/kotlin/com/example/todolistkotlin/repository/CardRepository.kt
+++ src/main/kotlin/com/example/todolistkotlin/repository/CardRepository.kt
@@ -1,9 +1,55 @@
 package com.example.todolistkotlin.repository
 
+import com.example.todolistkotlin.exception.CardNotFoundException
+import com.example.todolistkotlin.model.Card
+import org.springframework.stereotype.Service
 import javax.persistence.EntityManager
 import javax.persistence.PersistenceContext
 
+@Service
 class CardRepository {
+    // container managed entity manager
     @PersistenceContext
     private lateinit var entityManager: EntityManager
+
+    open fun createCard(): Card {
+        // todo no-arg plugin doesn't work this way?
+        val card = Card(title = "", description = "")
+
+        entityManager.run {
+            transaction.begin()
+            persist(card)
+            transaction.commit()
+        }
+
+        return card
+    }
+
+    open fun findAllCards(): List<Card> {
+        entityManager.transaction.begin()
+        val query = entityManager.createQuery("SELECT e FROM cards e")
+
+        val resultList = query.resultList
+        entityManager.transaction.commit()
+
+        return resultList as List<Card>
+    }
+
+    open fun findCardById(id: Long): Card {
+        entityManager.transaction.begin()
+        val card = entityManager.find(Card::class.java, id)
+            ?: throw CardNotFoundException("Card with id: $id not found")
+        entityManager.transaction.commit()
+
+        return card
+    }
+
+    open fun deleteCardById(id: Long) {
+        entityManager.transaction.begin()
+
+        val card = findCardById(id)
+        entityManager.remove(card)
+
+        entityManager.transaction.commit()
+    }
 }
